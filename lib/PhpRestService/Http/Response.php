5da9dbd7c8fce4ba9a9e1295995f57202bcd606e
Dirk Engels <d.engels@dirkengels.com>
2011-12-18 17:43:31 +0100

    added initial http classes

---
+++ lib/PhpRestService/Http/Response.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace PhpRestService\Http;
+
+class Response {
+
+    protected $_code = 501;
+    protected $_headers = array();
+    protected $_body = '';
+	
+    public function __construct() {
+        $this->addHeader('Server', 'PhpRestService v0.1');
+    }
+
+    public function getCode() {
+        return $this->_code;
+    }
+
+    public function setCode($code) {
+        $this->_code = $code;
+        return $this;
+    }
+
+    public function getHeaders() {
+        return $this->_headers;
+    }
+
+    public function setHeaders($headers) {
+        $this->_headers = $headers;
+        return $this;
+    }
+
+    public function addHeader($name, $value) {
+        $this->_headers[$name] = $value;
+        return $this;
+    }
+
+    public function removeHeader($name) {
+        unset($this->_headers[$name]);
+        return $this;
+    }
+
+    public function getBody() {
+        return $this->_body;
+    }
+
+    public function setBody($body) {
+        $this->_body = $body;
+    }
+
+
+    public function send() {
+        // Set Code
+        Header('HTTP/1.0 ' . $this->getCode() . Codes::get($this->getCode()));
+
+        // Set Headers
+        foreach($this->getHeaders() as $name => $value) {
+            Header($name . ': ' . $value);
+        }
+
+        // Show Body
+        echo $this->getBody();
+    }
+}
