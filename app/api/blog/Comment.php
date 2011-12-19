1bf9f5ba4f675189c266e6ed7adefa8e5b80931a
Dirk Engels <d.engels@dirkengels.com>
2011-12-19 01:27:36 +0100

    implemented blog comment api

--- app/api/blog/Comment.php
+++ app/api/blog/Comment.php
@@ -2,12 +2,13 @@
 
 class Comment {
 
+    protected $_model;
+
     public function write($data) {
-        
+        return $this->_model->insert($data);
     }
 
     public function load() {
-        
     }
 
     public function find($id) {
