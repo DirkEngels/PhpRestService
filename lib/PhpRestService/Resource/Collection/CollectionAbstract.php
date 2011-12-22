4a724c4a24ced690908e12b315e10b2524eda9ed
Dirk Engels <d.engels@dirkengels.com>
2011-12-22 21:24:45 +0100

    fixed throwing exceptions

--- lib/PhpRestService/Resource/Collection/CollectionAbstract.php
+++ lib/PhpRestService/Resource/Collection/CollectionAbstract.php
@@ -14,27 +14,27 @@ abstract class CollectionAbstract {
     }
 
     public function head() {
-        throw Exception('HTTP Method not implemented');
+        throw new \Exception('HTTP Method not implemented');
     }
 
     public function options() {
-        throw Exception('HTTP Method not implemented');
+        throw new \Exception('HTTP Method not implemented');
     }
 
     public function get() {
-        throw Exception('HTTP Method not implemented');
+        throw new \Exception('HTTP Method not implemented');
     }
 
     public function post() {
-        throw Exception('HTTP Method not implemented');
+        throw new \Exception('HTTP Method not implemented');
     }
 
     public function put() {
-        throw Exception('HTTP Method not implemented');
+        throw new \Exception('HTTP Method not implemented');
     }
 
     public function delete() {
-        throw Exception('HTTP Method not implemented');
+        throw new \Exception('HTTP Method not implemented');
     }
 
     public function handle() {
--- lib/PhpRestService/Resource/Item/ItemAbstract.php
+++ lib/PhpRestService/Resource/Item/ItemAbstract.php
@@ -16,27 +16,27 @@ abstract class ItemAbstract {
     }
 
     public function head() {
-        throw Exception('HTTP Method not implemented');
+        throw new \Exception('HTTP Method not implemented');
     }
 
     public function options() {
-        throw Exception('HTTP Method not implemented');
+        throw new \Exception('HTTP Method not implemented');
     }
 
     public function get() {
-        throw Exception('HTTP Method not implemented');
+        throw new \Exception('HTTP Method not implemented');
     }
 
     public function post() {
-        throw Exception('HTTP Method not implemented');
+        throw new \Exception('HTTP Method not implemented');
     }
 
     public function put() {
-        throw Exception('HTTP Method not implemented');
+        throw new \Exception('HTTP Method not implemented');
     }
 
     public function delete() {
-        throw Exception('HTTP Method not implemented');
+        throw new \Exception('HTTP Method not implemented');
     }
 
     public function handle() {
