60b7f13f4dcc7b4574b74bc5f036097bafdc6ed5
Dirk Engels <d.engels@dirkengels.com>
2011-12-23 00:40:43 +0100

    changed method accessibility

--- lib/PhpRestService/Resource/Formatter/All.php
+++ lib/PhpRestService/Resource/Formatter/All.php
@@ -4,11 +4,11 @@ namespace PhpRestService\Resource\Formatter;
 
 class All extends FormatterAbstract implements FormatterInterface {
 
-    protected static function dataBasic($object) {
+    public static function dataBasic($object) {
         return static::dataExtended($object);
     }
 
-    protected static function dataExtended($object) {
+    public static function dataExtended($object) {
         $data = array();
         if (method_exists($object, 'toArray')) {
             $data = $object->toArray();
