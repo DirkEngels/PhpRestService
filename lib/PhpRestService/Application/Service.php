79e011438b586d503ab3012df5dc399c2516b293
Dirk Engels <d.engels@dirkengels.com>
2011-12-23 02:55:06 +0100

    used factory in the appliction service

--- lib/PhpRestService/Application/Service.php
+++ lib/PhpRestService/Application/Service.php
@@ -15,44 +15,7 @@ class Service extends ApplicationAbstract implements ApplicationInterface {
     protected function _loadResource($router) {
         $resourceName = $router->getResource();
 
-        $resourceManager = new \PhpRestService\Resource\Manager\ManagerDefault();
-        $baseClass = '\\App\\Service\\' . $resourceName;
-
-        // Set formatter
-        if (class_exists($baseClass . '\\Display')) {
-            $formatterClass = $baseClass . '\\Display';
-            \PhpRestService\Logger::get()->log('Setting custom formatter: ' . $formatterClass, \Zend_Log::INFO);
-        } else {
-            $formatterClass = '\\PhpRestService\\Resource\\Display\\All';
-            \PhpRestService\Logger::get()->log('Setting default formatter: ' . $formatterClass, \Zend_Log::INFO);
-        }
-        $formatter = new $formatterClass();
-        $resourceManager->setDisplay($formatter);
-
-        $matches = array();
-        if (preg_match('#/([0-9]+)$#', ($_SERVER['REQUEST_URI']), $matches)) {
-            // Item resource
-            \PhpRestService\Logger::get()->log('Trying item resource: GET: ' . $resourceName, \Zend_Log::INFO);
-            if (class_exists($baseClass . '\\Item')) {
-                $resourceManager->setId($matches[0]);
-                \PhpRestService\Logger::get()->log('Loading item resource: GET: ' . $resourceName, \Zend_Log::INFO);
-
-                $itemClass = $baseClass . '\\Item';
-                $item = new $itemClass();
-                $resourceManager->setItem($item);
-            }
-        } else {
-            \PhpRestService\Logger::get()->log('Trying collection resource: GET: ' . $baseClass . '\\Collection', \Zend_Log::INFO);
-            // Collection resource
-            if (class_exists($baseClass . '\\Collection')) {
-                \PhpRestService\Logger::get()->log('Loading collection resource: GET: ' . $resourceName, \Zend_Log::INFO);
-
-                $collectionClass = $baseClass . '\\Collection';
-                $collection = new $collectionClass();
-                $resourceManager->setCollection($collection);
-            }
-        }
-
+        $resourceManager = \PhpRestService\Resource\Factory::get($resourceName);
         return $resourceManager;
     }
 
