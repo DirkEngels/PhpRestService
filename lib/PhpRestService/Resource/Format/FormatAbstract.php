4281d2676700874ea03a6d65b483f8f6eb52a080
Dirk Engels <d.engels@dirkengels.com>
2011-12-28 02:44:25 +0100

    added getter & setter for the response object

--- lib/PhpRestService/Resource/Format/FormatAbstract.php
+++ lib/PhpRestService/Resource/Format/FormatAbstract.php
@@ -10,4 +10,12 @@ abstract class FormatAbstract {
         $this->_response = new \PhpRestService\Http\Response();
     }
 
+    public function getResponse() {
+        return $this->_response;
+    }
+
+    public function setResponse($response) {
+        $this->_respnse = $response;
+        return $this;
+    }
 }
