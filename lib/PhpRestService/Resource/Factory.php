19079d3f9084d092a362e2a88c56769ec9bff36f
Dirk Engels <d.engels@dirkengels.com>
2011-12-28 03:31:58 +0100

    pass id

--- lib/PhpRestService/Resource/Factory.php
+++ lib/PhpRestService/Resource/Factory.php
@@ -15,7 +15,7 @@ class Factory {
      * @param $resourceName
      * @return \PhpRestService\Resource\Manager\ManagerAbstract
      */
-    public static function get($resourceName) {
+    public static function get($resourceName, $resourceKey = NULL) {
         $msg = 'FACTORY: Getting resource: ' . $resourceName;
         \PhpRestService\Logger::log($msg, \Zend_Log::DEBUG);
         \PhpRestService\Logger::log('----------', \Zend_Log::DEBUG);
@@ -28,7 +28,7 @@ class Factory {
             self::getComponentType($resourceName, self::TYPE_COLLECTION)
         );
         $manager->setItem(
-            self::getComponentType($resourceName, self::TYPE_ITEM)
+            self::getManagerDataItem($resourceName, $resourceKey)
         );
 
         // Display
@@ -88,7 +88,7 @@ class Factory {
      * @return \PhpRestService\Resource\Manager\Data\DataAbstract
      */
     public static function getManagerDataCollection($resourceName) {
-        return self::getComponentType($resourceName, self::TYPE_DATA_COLLECTION);
+        return self::getComponentType($resourceName, self::TYPE_COLLECTION);
     }
 
     /**
@@ -96,8 +96,12 @@ class Factory {
      * @param string $resourceName
      * @return \PhpRestService\Resource\Manager\Data\DataAbstract
      */
-    public static function getManagerDataItem($resourceName, $id) {
-        return self::getComponentType($resourceName, self::TYPE_DATA_ITEM);
+    public static function getManagerDataItem($resourceName, $id = NULL) {
+        $item = self::getComponentType($resourceName, self::TYPE_ITEM);
+        if (!is_null($id)) {
+            $item->setId($id);
+        }
+        return $item;
     }
 
     /**
