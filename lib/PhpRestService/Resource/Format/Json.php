65143b42a216059054ee3406315eb7f02db1cc20
Dirk Engels <d.engels@dirkengels.com>
2011-12-29 03:53:05 +0100

    remove default http code

--- lib/PhpRestService/Resource/Format/Json.php
+++ lib/PhpRestService/Resource/Format/Json.php
@@ -10,7 +10,6 @@ class Json extends FormatAbstract implements FormatInterface {
 
     public function render($data) {
         $this->getResponse()
-            ->setCode(200)
             ->addHeader('Content-type', 'application/json')
             ->setBody(
                 \Zend_Json::encode($data)
