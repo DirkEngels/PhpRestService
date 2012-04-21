fd57697d03d9d8f8e8681bf8bd203d98a2b00db5
Dirk Engels <dirk.engels@booking.com>
2012-04-21 19:05:38 +0200

    new method meta

--- lib/PhpRestService/Resource/Display/DisplayAbstract.php
+++ lib/PhpRestService/Resource/Display/DisplayAbstract.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace PhpRestService\Resource\Display;
+
 use PhpRestService\Config;
 
 use \PhpRestService\Resource\Component;
@@ -8,9 +9,15 @@ use \PhpRestService\Logger;
 
 abstract class DisplayAbstract extends Component\ComponentAbstract {
 
+    const URL = '';
+
     protected $_url;
 
     public function getUrl() {
+        $class = get_class( $this );
+        if ( is_null( $this->_url ) && $class::URL != '' ) {
+            $this->_url = 'http://' . $_SERVER['HTTP_HOST'] . $class::URL;
+        }
         if (is_null($this->_url)) {
             $this->setUrl('http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
         }
@@ -54,13 +61,20 @@ abstract class DisplayAbstract extends Component\ComponentAbstract {
                 $url .= '/' . $id;
                 $data = array (
                     'id' => $object->getId(),
-                    'url' => $url,
+                    'link' => $url,
                 );
             }
         }
         return $data;
     }
 
+    /**
+     * 
+     * @param Object $object
+     * @param boolena $extended
+     * @param string $url
+     * @return array
+     */
     public function displayItem($object, $extended = false, $url = true) {
         // Basic data
         $data = $this->dataBasic($object);
@@ -86,14 +100,19 @@ abstract class DisplayAbstract extends Component\ComponentAbstract {
         $data = array();
 
         if ( is_array($objects) || get_class($objects) == 'Doctrine\ORM\PersistentCollection') {
-            foreach($objects as $object) {
-                if (method_exists($object, 'getId')) {
+            foreach($objects as $key => $object) {
+                if ( ! method_exists($object, 'getId')) {
+                    $msg = "Displaying item: " . get_class($object) . ' without ID';
+                } else {
                     $msg = "Displaying item: " . get_class($object) . ' => ' . $object->getId();
-                    Logger::log( $msg, \Zend_Log::DEBUG );
                 }
+                Logger::log( $msg, \Zend_Log::DEBUG );
 
-                array_push($data, $this->displayItem($object, $extended, $url) );
-
+                if ( preg_match( '/[a-zA-Z0-9]+/', $key ) ) {
+                    $data[ $key ] = $this->displayItem($object, $extended, $url);
+                } else {
+                    array_push($data, $this->displayItem($object, $extended, $url) );
+                }
             }
         } elseif ( is_object( $objects ) ) {
             $data = $this->displayItem( $objects );
@@ -122,7 +141,15 @@ abstract class DisplayAbstract extends Component\ComponentAbstract {
         return $data;
     }
 
+
+    public function dataMeta( $object ) {
+        return ( $_REQUEST['meta'] == 1 ) ? $this->dataMeta() : array();
+    }
+
+
     public function handle($inputData = array(), $extended = NULL) {
+        $display = $this->dataMeta();
+
         if ($this->getId()) {
             $extended = (!is_null($extended)) ? $extended : TRUE;
             $display = $this->displayItem($inputData, $extended);
