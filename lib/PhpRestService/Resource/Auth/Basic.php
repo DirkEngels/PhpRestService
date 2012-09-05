44782cf06c38b9f097f0629ad9437b69f9faa033
Dirk Engels <dirk.engels@booking.com>
2012-09-06 00:38:47 +0200

    create PhpEventLog events

--- lib/PhpRestService/Resource/Auth/Basic.php
+++ lib/PhpRestService/Resource/Auth/Basic.php
@@ -5,9 +5,11 @@ namespace PhpRestService\Resource\Auth;
 class Basic extends AuthAbstract implements AuthInterface {
 
     public function authenticate() {
+        \PhpEventLog\Log\Collector::add( new \PhpEventLog\Event\User() );
         if ((isset($_SERVER['PHP_AUTH_USER'])) && (isset($_SERVER['PHP_AUTH_PW']))) {
             if (($_SERVER['PHP_AUTH_USER'] == 'dirk') && ($_SERVER['PHP_AUTH_PW'] == 'engels')) {
                 // Authenticated: proceed!
+                \PhpEventLog\Log\Collector::add( new \PhpEventLog\Event\User( $user, 1 ) );
                 return TRUE;
             }
         }
